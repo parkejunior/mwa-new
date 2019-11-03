37f5e1aad7397a23ae721e97bed5daff73f0f54e
Patrick Luan <patrickluan360@gmail.com>
2019-11-03 01:46:14 -0300

    Upload website files to repo

---
+++ public/index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>MWA Sistemas</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- BOOTSTRAP -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+
+	<link rel="stylesheet" type="text/css" href="/css/estilo.css">
+	<script type="text/javascript" src="/js/script.js"></script>
+</head>
+<body>
+	<header class="bg-datacenter">
+		<nav class="navbar navbar-expand-sm navbar-dark justify-content-between" id="menuNav">
+			<a href="#"><img src="/img/mwa-logo-new-white.png" class="img-fluid" style="max-height: 60px;"></a>
+			<ul class="navbar-nav align-items-stretch">
+
+				<li class="nav-item active"><a class="nav-link" href="#">Início</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Empresa</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Soluções</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Diferenciais</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Segmentos</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Suporte</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Contato</a></li>
+				<li class="btn btn-primary ml-2"><a class="nav-link text-white" href="#"><i class="fas fa-user"></i> Área do Cliente</a></li>
+			</ul>
+		</nav>
+
+		<div class="text-white container text-center text-uppercase jumbotron bg-transparent text-break">
+			<h1 class="display-2">Soluções Tecnológicas Inteligentes</h1>
+		</div>
+		<div class="text-white text-center display-3"><i class="fas fa-chevron-down"></i></div>
+
+		<svg id="top-section-border" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+	    <polygon fill="#0a3d62" points="0,100 100,0 100,100"/>
+	 	</svg>
+	</header>
+	<section class="bg-blue text-white">
+		<div class="container jumbotron bg-transparent  mb-0">
+		<div class="row">
+		<div class="col-sm-3">
+			<div class="nav flex-column nav-pills" role="tablist" aria-orientation="vertical" id="pill-erp">
+				<a class="nav-link active" id="pill-erp-mwa-tab" data-toggle="pill" role="tab" aria-controls="pill-erp-mwa" aria-selected="true" href="#pill-erp-mwa">O que é o software de gestão ERP-MWA?</a>
+				<a class="nav-link" id="pill-erp-tab" data-toggle="pill" role="tab" aria-controls="pill-werp" aria-selected="true" href="#pill-werp">O que é ERP?</a>
+				<a class="nav-link" id="pill-e-commerce-tab" data-toggle="pill" role="tab" aria-controls="pill-e-commerce" aria-selected="true" href="#pill-e-commerce">e-Commerce ERP-MWA</a>
+			</div>
+		</div>
+		<div class="col tab-content">
+
+		<div  class="tab-pane fade show active" role="tabpanel" aria-labelledby="pill-erp-mwap" id="pill-erp-mwa">
+		<h1>ERP-MWA</h1>
+
+		<p>ERP-MWA é um Software de Gestão totalmente Web para pequenas e médias empresas, com grande tecnologia agregada, o sistema corresponde do comércio a indústria. Integração garantida de todos os seus estabelecimentos em uma única solução. Direcione o Frente de Caixa, o E-Commerce, o aplicativo para Dispositivos Móveis; tudo em um único repositório para o melhor gerenciamento.</p>
+
+		<p>O Software Online ERP-MWA é a melhor alternativa para informatizar e integrar as atividades administrativas, financeiras, comercial e produção industrial ou controle de serviços, de acordo com as melhores práticas de processos empresariais do mercado.</p>
+
+		<p>Com o ERP-MWA sua empresa será controlada de forma adequada a sua necessidade, fornecendo-lhe segurança e facilidade para tomadas de decisão, com um suporte técnico preparado e especializado.</p>
+		</div>
+
+		<div  class="tab-pane fade" role="tabpanel" aria-labelledby="pill-werp" id="pill-werp">
+		<h1>O que é ERP?</h1>
+
+		<p>Sistemas Integrados de Gestão Empresarial (SIGE ou SIG), em inglês Enterprise Resource Planning (ERP), são sistemas de informação que integram todos os dados e processos de uma organização em um único sistema. A integração pode ser vista sob a perspectiva funcional (sistemas de: finanças, contabilidade, recursos humanos, fabricação, marketing, vendas, compras, etc) e sob a perspectiva sistêmica (sistema de processamento de transações, sistemas de informações gerenciais, sistemas de apoio à decisão, etc).</p>
+		</div>
+
+		<div  class="tab-pane fade" role="tabpanel" aria-labelledby="pill-e-commerce" id="pill-e-commerce">
+		<h1>e-Commerce</h1>
+
+		<p>Imagine seus clientes serem atraidos pela facilidade e possibilidade de abertura de pedido de venda. Esse é o nosso objetivo com o carrinho de compra, tornar seu produto mais atrativo. Para isso conta com integração completa com o ERP-MWA, o que permite o acompanhamento em tempo real dos seus pedidos e estoque dos produtos.</p>
+
+		<p>Utilizando uma loja virtual você potencializa suas vendas com total segurança e controle, sem se preocupar com a perda de informações.</p>
+
+		<span>Características do e-Commerce:</span>
+
+		<ul>
+			<li>Loja Virtual integrada com o ERP-MWA</li>
+			<li>Esquema de cores customizável</li>
+			<li>Pagamento através do PagSeguro</li>
+		</ul>
+		</div>
+
+		</div>
+		</div>
+		</div>
+
+		<svg id="top-section-border" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+	    <polygon fill="#3c6382" points="0,100 50,0 100,100"/>
+	 	</svg>
+	</section>
+
+	<section class="">
+		<div class="container jumbotron bg-transparent">
+		<h1 class="mb-4 text-center">ALGUMAS EMPRESAS QUE UTILIZAM O ERP-MWA</h1>
+
+		<div class="row">
+		<div class="col d-flex"><img src="/img/logo-sokit.png" class="img-fluid align-self-center" ></div>
+		<div class="col d-flex"><img src="/img/logo-rozecar.png" class="img-fluid align-self-center" ></div>
+		<div class="col d-flex"><img src="/img/logo-brasil.png" class="img-fluid align-self-center" ></div>
+		<div class="col d-flex"><img src="/img/logo-dunquer.png" class="img-fluid align-self-center" ></div>
+		<div class="col d-flex"><img src="/img/logo-gira.png" class="img-fluid align-self-center" ></div>
+		</div>
+		</div>
+	</section>
+
+	<footer class="bg-dark text-white">
+		<div class="container justify-content-between p-4">
+			
+		Home     Empresa     Produtos     ERP - MWA     Notícias     Contato
+		<span>2000 - 2019 - MWA Sistemas - Direitos Reservados ©</span>
+
+		<a href="">Localização (GoogleMaps)</a>
+		Rua Goes Monteiro 410 | Joinville - SC - Brasil | CEP 89206-320 | Telefone: (47)3028-7065
+		</div>
+	</footer>
+</body>
+</html>
